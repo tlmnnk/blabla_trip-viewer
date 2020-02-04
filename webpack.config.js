2ddc854a6639344b819eb376aa9c067736aee8e6
Vladislav T <vt118@mail.ru>
2020-02-04 14:19:54 +0300

    feat: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+import path from 'path';
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        polyfill: 'babel-polyfill',
+        app: './js/app.js'
+    },
+    context: path.resolve(_dirname, 'src'),
+    module: {
+        rules: [
+            {
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+                test: /\.js$/,
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: [name].js
+    },
+    devServer: {
+        publicPath: '/',
+        port: 9000,
+        contentBase: path.join(process.cwd(), 'dist'),
+        host: 'localhost',
+        historyApiFallback: true,
+        noInfo: false,
+        stats: 'minimal',
+        hot: true,
+      },
+};
