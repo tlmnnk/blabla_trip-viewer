b47838bf9967100cf632227f64d8599d4f7bb747
vladislav t <vt118@mail.ru>
2020-02-07 20:35:58 +0300

    feat: develop function and class variable that gets sorted list for autocomplete in trips.js

--- src/js/store/trips.js
+++ src/js/store/trips.js
@@ -5,9 +5,50 @@ class Trips {
         this.myTrips = null;
     }
 
-    async init(params) {
-        const response = await Promise.all([api.getTrips(params)]);
-        this.myTrips = response.data.trips;
+    async init() {
+        const areas = await this.api.getAreas();
+        const russianAreas = this.getRussianAreas(areas).areas;
+        this.citiesAutocompleteList = this.getCitiesAutocompleteList(russianAreas);
+    }
+    getRussianAreas(countries) {
+        return countries.filter(country => country.id == '113')[0];
+    }
+
+    getRidOfBracesInCityName(city) {
+        if(city.name.includes('(')) {
+            const index = city.name.indexOf('(');
+            city.name = city.name.substr(0, index - 1);
+        }
+    }
+
+    addMoscowAndPeterburg(array) {
+        array.unshift(['2', 'Санкт-Петербург']);
+        array.unshift(['1', 'Москва']);
+        return array;
+    }
+   getSerializedCities(areas) {
+    return areas.reduce((acc, item)=>{
+        let cities = {};
+        const region = item.name;
+        item.areas.forEach(city => {
+            this.getRidOfBracesInCityName(city);
+            cities[city.id] = `${city.name}, ${region}`;
+        });
+        Object.assign(acc, cities);
+        return acc;
+    }, {});
+   }
+    // 
+    getCitiesAutocompleteList(areas) {
+        let citiesTemp = getSerializedCities(areas); 
+        let citiesSorted = Object.entries(citiesTemp).sort((a,b) => a - b);
+
+        citiesSorted = this.addMoscowAndPeterburg(citiesSorted);
+        
+        return citiesSorted.reduce((acc, item) => {
+            acc[item[1]] = null;
+            return acc;
+        }, {});
     }
 }
 
