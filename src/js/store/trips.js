b00915084f63892e10703d54624567b04de266a4
Vladislav T <vt118@mail.ru>
2020-02-07 14:51:53 +0300

    feat: add init to trips, not complete

--- src/js/store/trips.js
+++ src/js/store/trips.js
@@ -2,12 +2,30 @@ import api from '../services/apiService';
 
 class Trips {
     constructor(api) {
+        this.api = api;
         this.myTrips = null;
     }
 
-    async init(params) {
-        const response = await Promise.all([api.getTrips(params)]);
-        this.myTrips = response.data.trips;
+    async init() {
+        const areas = await this.api.getAreas();
+        this.russianAreas = this.getRussianAreas(areas).areas;
+        this.cities = this.getCities(this.russianAreas);
+    }
+    getRussianAreas(countries) {
+        return countries.filter(country => country.id == '113')[0];
+    }
+    // 
+    getCities(areas) {
+      return areas.reduce((acc, item)=>{
+            let cities = {};
+            item.areas.forEach(city => {
+                cities[city.name] = null;
+            });
+            Object.assign(acc, cities);
+            return acc;
+        }, {});
+
+       
     }
 }
 
